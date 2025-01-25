bf41bcb98b079bb3a42faea72e5951229618e7a1
daken <2844775971@qq.com>
2025-01-25 21:15:02 +0800

    fix 跨域

---
+++ backend/src/main/java/com/tailYY/backend/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.tailYY.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author daken 2025/1/25
+ **/
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // 对所有路径生效
+                        .allowedOrigins("*") // 允许的源
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 允许的方法
+                        .allowedHeaders("*") // 允许的头信息
+                        .allowCredentials(true); // 是否支持凭证
+            }
+        };
+    }
+}
