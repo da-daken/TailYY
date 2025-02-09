7acd015c322843d580b8743103b6bd70a2063bc4
daken <2844775971@qq.com>
2025-02-09 22:59:19 +0800

    add 上传照片

---
+++ backend/src/main/java/com/tailYY/backend/controller/FileUploadController.java
@@ -0,0 +1,58 @@
+package com.tailYY.backend.controller;
+
+import com.tailYY.backend.common.constants.ErrorCode;
+import com.tailYY.backend.common.exception.BusinessException;
+import com.tailYY.backend.common.response.BaseResponse;
+import com.tailYY.backend.common.util.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * @author daken 2025/2/9
+ **/
+@RestController
+public class FileUploadController {
+    private static final String UPLOAD_DIR = "backend/photos";
+
+    @PostMapping("/uploadPhoto")
+    public BaseResponse<Boolean> uploadPhoto(@RequestParam("photo") MultipartFile file) {
+        if (file.isEmpty()) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+
+        try {
+            // 确保保存目录存在
+            File dir = new File(UPLOAD_DIR);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+
+            // 获取文件名并构建目标路径
+            String fileName = file.getOriginalFilename();
+            Path path = Paths.get(UPLOAD_DIR + fileName);
+
+            // 将文件保存到指定位置
+            Files.copy(file.getInputStream(), path);
+
+            return ResultUtils.success(true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return ResultUtils.success(false);
+    }
+
+    @GetMapping("/test")
+    public void test() {
+        System.out.println("Current working directory: " + System.getProperty("user.dir"));
+    }
+}
